4eb9acee0b13823e36f951a7c56cf0eaff3e34a0
gceni <gceni10@gmail.com>
2025-07-10 17:16:02 -0300

    versao modificada para Google Cloud Run

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import gradio as gr
+import os
 from pages.theme import softCIF
 from pages.main import view as main_page
 from pages.about import view as about_page
@@ -12,6 +13,8 @@ with demo.route("Contato", "feedback"):
     feedback_page.interface.render()
 
 # Utilizar gradio app.py para modo com reload
+# A linha abaixo garante que o Gradio use a porta fornecida pelo Cloud Run
+# e que seja acessível de fora do contêiner.
 if __name__ == "__main__":
     print("Executando a aplicação...")
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 8080)))
